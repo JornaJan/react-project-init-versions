3901eb4d11752fe6722b84ee9cec6779eca21229
JornaJan <loverong09@126.com>
2022-11-11 16:52:04 +0800

    React v17.0.2 & react-router v5.1.2 & react-scripts v3.4.0

---
+++ README.md
@@ -0,0 +1,11 @@
+### React Project Versions init
+
+> react v17.0.2
+> react-dom v17.0.2
+
+> react-scripts v3.4.0
+
+> web-vitals v1.1.2
+
+> react-router v5.1.2
+
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Version 17.0.2</h1>
+      <p>react-scripts v3.4.0</p>
+      <h3>hello</h3>
+    </div>
+  );
+}
+
+export default App;
